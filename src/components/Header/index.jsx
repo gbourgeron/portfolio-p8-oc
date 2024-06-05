e7f3c50c2d53b76fd89024e0469b0d1a6fcdadb8
Gurvan B <gbourgeron@gmail.com>
2024-06-05 18:01:33 +0200

    HEADER & HOME style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import './index.scss';
 
 function Header() {
     const location = useLocation();
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
+import './index.scss';
+
 function Home() {
     return (
         <main>
-            ACCUEIL
+            <div className='titles'>
+                <h2>GURVAN BOURGERON</h2>
+                <h1>DÉVELOPPEUR WEB</h1>
+            </div>
+            <p className='text-presentation'>&lt;Autodidacte et passionné par les challenges, mon objectif est d’accomplir les missions que vous me confierez,
+si toutefois je l’accepte. /&gt;</p>
         </main>
     );
 }
