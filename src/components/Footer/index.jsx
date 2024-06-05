2ff46293f92024c9022cf9f72fcb6fb4cee89a52
Gurvan B <gbourgeron@gmail.com>
2024-06-05 17:22:50 +0200

    INSTALL SASS + FOOTER first style and content

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,12 @@
-function Footer() {
+import './index.scss';
 
+function Footer() {
+    const thisYear = new Date().getFullYear();
+    return (
+        <footer>
+            <p>&copy; {thisYear} Gurvan Bourgeron - Tous droits réservés.</p>
+        </footer>
+    )
 }
 
 export default Footer;
