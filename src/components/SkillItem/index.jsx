de8c5703d82a3d6bdf8e553ce71fb552a914e055
Gurvan B <gbourgeron@gmail.com>
2024-06-06 15:57:29 +0200

    TOOLS skills added + gestion projet and soft skills

--- src/components/SkillItem/index.jsx
+++ src/components/SkillItem/index.jsx
@@ -1,14 +1,25 @@
 import './index.scss';
+import checkIcon from '../../assets/check.svg';
 
-function SkillItem({ name, level}) {
+function SkillItem({ name, level, check}) {
     return (
         <li className="skill-item">
             <span className="skill-name">{name}</span>
-            <div className="skill-level">
+            {/* <div className="skill-level">
                 {[...Array(5)].map((_, i) => (
                     <div key={i} className={`rectangle ${i < level ? 'filled' : ''}`}></div>
                 ))}
-            </div>
+            </div> */}
+              {level !== undefined && (
+                <div className="skill-level">
+                    {[...Array(5)].map((_, i) => (
+                        <div key={i} className={`rectangle ${i < level ? 'filled' : ''}`}></div>
+                    ))}
+                </div>
+            )}
+            {check !== undefined && check && (
+                <img src={checkIcon} alt="Check" className="check-icon" />
+            )}
         </li>
     );
 }
--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -16,7 +16,7 @@ function Skills() {
             <div className="box-skills">
                 <div className="title-box">LANGAGES & TECHNOLOGIES</div>
                 <div className="skills">
-                    <div className="block-skills frontend">
+                    {/* <div className="block-skills frontend">
                         <ul className='skill-list'>
                             {skills.slice(0, 4).map(skill => (
                                     <SkillItem key={skill.name} name={skill.name} level={skill.level} />
@@ -27,6 +27,49 @@ function Skills() {
                         <ul className='skill-list'>
                             {skills.slice(4).map(skill => (
                                 <SkillItem key={skill.name} name={skill.name} level={skill.level} />
+                            ))}
+                        </ul>
+                    </div> */}
+                    <div className="block-skills">
+                        <ul className="skill-list">
+                            {skills.languages && skills.languages.map(skill => (
+                                <SkillItem key={skill.name} name={skill.name} level={skill.level} />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="box-skills">
+                <div className="title-box">OUTILS</div>
+                <div className="skills">
+                    <div className="block-skills">
+                        <ul className="skill-list">
+                            {skills.tools && skills.tools.map(tool => (
+                                <SkillItem key={tool.name} name={tool.name} check={tool.check} />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="box-skills">
+                <div className="title-box">GESTION DE PROJET</div>
+                <div className="skills">
+                    <div className="block-skills">
+                        <ul className="skill-list">
+                            {skills.projectManagement && skills.projectManagement.map(tool => (
+                                <SkillItem key={tool.name} name={tool.name} check={tool.check} />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="box-skills">
+                <div className="title-box">SOFT SKILLS</div>
+                <div className="skills">
+                    <div className="block-skills">
+                        <ul className="skill-list">
+                            {skills.softSkills && skills.softSkills.map(tool => (
+                                <SkillItem key={tool.name} name={tool.name} check={tool.check} />
                             ))}
                         </ul>
                     </div>
