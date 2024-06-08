1765942f0b0938606b7eda9abf5e2814ee2e99fa
Gurvan B <gbourgeron@gmail.com>
2024-06-08 17:42:18 +0200

    RESPONSIVE part 2

--- src/components/SkillItem/index.jsx
+++ src/components/SkillItem/index.jsx
@@ -5,11 +5,6 @@ function SkillItem({ name, level, check}) {
     return (
         <li className="skill-item">
             <span className="skill-name">{name}</span>
-            {/* <div className="skill-level">
-                {[...Array(5)].map((_, i) => (
-                    <div key={i} className={`rectangle ${i < level ? 'filled' : ''}`}></div>
-                ))}
-            </div> */}
               {level !== undefined && (
                 <div className="skill-level">
                     {[...Array(5)].map((_, i) => (
