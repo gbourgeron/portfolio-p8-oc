2bad0f55be2d336905c347de3c4fb44a80f71e82
Gurvan B <gbourgeron@gmail.com>
2024-07-09 15:21:24 +0200

    AUTOSCROLL TOP for pages

--- src/components/AppRouter/index.jsx
+++ src/components/AppRouter/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "../Layout";
+import ScrollToTop from "../ScrollToTop";
 
 import Contact from "../../pages/Contact";
 import Error from "../../pages/Error";
@@ -12,6 +13,7 @@ import Skills from "../../pages/Skills";
 function AppRouter() {
     return (
         <Router>
+            <ScrollToTop />
             <Layout>
                 <Routes>
                     <Route
